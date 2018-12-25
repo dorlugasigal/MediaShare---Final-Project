6c22c7609e576067aa8b6aabc5b6664c9d2a93ad
unknown <danielsaka2@gmail.com>
2018-12-25 21:21:07 +0200

    Menu-added

--- src/screens/menu/menu.js
+++ src/screens/menu/menu.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import {Button, View, Text,StyleSheet } from 'react-native';
+import {Button, View, Text,StyleSheet,TouchableOpacity } from 'react-native';
 import Icon from "react-native-vector-icons/MaterialIcons"
-
+import Icon2 from "react-native-vector-icons/AntDesign"
 const styles = StyleSheet.create({
     rowmenu: {
       flexDirection: 'row',
       height: '33%',
       justifyContent: 'space-evenly', 
     },
+    buttons:{
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '40%',
+      height:'80%',    
+    },
+    text:{
+      fontSize: 20,
+      fontWeight: 'bold',
+    }
   
   });
 
@@ -16,37 +26,57 @@ class MenuScreen extends React.Component {
       return (
         <View style={{ flex: 1, }}>
         <View style={styles.rowmenu}> 
-          <Button
-          title="Documents"
-          onPress={()=>this.props.navigation.navigate('Details')}
-          />
-  
-          <Button
-            title="Audio"
-            onPress={() => this.props.navigation.navigate('Details')}>
-              <Icon name="audiotrack" size={30} color='white'/>
-            </Button>
+          {/*Documents button*/}
+          <TouchableOpacity
+          style={styles.buttons}
+          onPress={()=>this.props.navigation.navigate('Details')}>
+          <Text style={styles.text}>Documents</Text>
+          <Icon2 name="addfile" size={40} color='black'/>
+          </TouchableOpacity>
+  {/*Audio button*/}
+          <TouchableOpacity
+            style={styles.buttons}
+            onPress={() => this.props.navigation.navigate('Details')}
+            >
+            <Text style={styles.text}>Audio</Text>
+              <Icon name="audiotrack" size={40} color='black'/>
+            </TouchableOpacity>
           </View>
           <View style={styles.rowmenu}> 
-
-          <Button
-            title="Gallery"
+          {/*Gallery button*/}
+          <TouchableOpacity
+            style={styles.buttons}
             onPress={() => this.props.navigation.navigate('Details')}
-          />
-          <Button
-            title="Schedule"
+          >
+          <Text style={styles.text}>Gallery</Text>
+          <Icon name="photo-library" size={40} color='black'/>
+          </TouchableOpacity>
+          {/*Schedule button*/}
+          <TouchableOpacity
+            style={styles.buttons}
             onPress={() => this.props.navigation.navigate('Details')}
-          />
+          >
+          <Text style={styles.text}>Schedule</Text>
+          <Icon name="schedule" size={40} color='black'/>
+          </TouchableOpacity>
           </View>
           <View style={styles.rowmenu}> 
-          <Button
-            title="Settings"
+          {/*Settings button*/}
+          <TouchableOpacity
+            style={styles.buttons}
             onPress={() => this.props.navigation.navigate('Details')}
-          />
-          <Button
-            title="Uploaded Files"
+          >
+          <Text style={styles.text}>Settings</Text>
+          <Icon name="settings" size={40} color='black'/>
+          </TouchableOpacity>
+          {/*Uploaded Files button*/}
+          <TouchableOpacity
+            style={styles.buttons}
             onPress={() => this.props.navigation.navigate('Details')}
-          />
+          >
+          <Text style={styles.text}>Uploaded Files</Text>
+          <Icon name="cloud-upload" size={40} color='black'/>
+          </TouchableOpacity>
           </View>
         </View>
       );
