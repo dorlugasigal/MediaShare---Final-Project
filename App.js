6a591f7b5716de3fb4091b5b4f571d8781747093
Dor Lugasi-Gal <dorlugasigal@gmail.com>
2019-05-06 21:09:52 +0300

    changed the design of the groups page

--- App.js
+++ App.js
@@ -129,7 +129,7 @@ const AppContainer = createAppContainer(createSwitchNavigator(
     MainScreen: AppStack
   },
   {
-    initialRouteName: 'LoginSplashScreen',
+    initialRouteName: 'MainScreen',
   }
 ));
 
--- src/Screens/Menu/scrGroups.js
+++ src/Screens/Menu/scrGroups.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import {withNavigation } from 'react-navigation';
+import { withNavigation } from 'react-navigation';
 import { TouchableOpacity, FlatList, View, Text, StyleSheet, Button } from 'react-native'
 const GLOBAL = require('../../Globals.js');
 import Icon from "@expo/vector-icons/Ionicons"
+import { Dimensions } from "react-native";
 
+var width = Dimensions.get('window').width; //full width
+var height = Dimensions.get('window').height; //full height
 
 class MyListItem extends React.PureComponent {
   constructor(props) {
     super(props);
   }
+  
   _onPress = () => {
     this.props.onPressItem(this.props.name);
   };
@@ -18,11 +22,12 @@ class MyListItem extends React.PureComponent {
   render() {
     return (
       <View style={styles.contianer}>
-        <TouchableOpacity onPress={this._onPress}>
-          <Text style={styles.groupName}>{this.props.name}</Text>
+        <TouchableOpacity onPress={this._onPress} style={styles.groupName} >
+          <Text style={{ marginTop: 12, marginLeft: 15, fontWeight: 'bold', color: '#fff', fontSize: 20, }}>{this.props.name}</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={this._onPressRemove}>
-          <Text style={styles.removeGroup}>Remove Group<Icon name="ios-trash" size={25} /></Text>
+
+        <TouchableOpacity onPress={this._onPressRemove} style={styles.removeGroup} >
+          <Icon style={{ marginTop: 13, marginLeft: 22, color: '#ccebff' }} name="ios-trash" size={30} />
         </TouchableOpacity>
       </View>
     );
@@ -43,7 +48,7 @@ class GroupsScreen extends React.Component {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        'groupAdmin': global.email
+        'groupAdmin': 'dorlugasigal@gmail.com'//global.email
       })
     })
       .then((response) =>
@@ -94,13 +99,10 @@ class GroupsScreen extends React.Component {
   componentDidMount() {
 
     this.focusListener = this.props.navigation.addListener("didFocus", () => {
-      this.getGroups();    
+      this.getGroups();
     });
-<<<<<<< HEAD
-    
 
-=======
->>>>>>> 08783cc35df9e04ca0800bca878b3f6ee12be06d
+
     this.getGroups();
   }
   _keyExtractor = (item, index) => item._id;
@@ -126,32 +128,58 @@ class GroupsScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>GroupsScreen</Text>
         <FlatList
           data={this.state.groups}
           extraData={this.state.groups}
           keyExtractor={this._keyExtractor}
           renderItem={this._renderItem}
         />
-        <Button title={'Add Group'} style={styles.bottomStyle} onPress={() => this._addGroup()}></Button>
+        <TouchableOpacity
+          style={{
+            borderWidth: 1,
+            borderColor: 'rgba(0,0,0,0.2)',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: 70,
+            height: 70,
+            marginBottom:10,
+            backgroundColor: '#fff',
+            borderRadius: 50,
+          }}
+          onPress={() => this._addGroup()}
+        >
+          <Icon name={"ios-add"} size={30} color="#ccebff" />
+        </TouchableOpacity>
+        {/* <Button title={'Add Group'} style={styles.bottomStyle} ></Button> */}
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
   contianer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    margin: 10,
+    width: width - 30,
+    backgroundColor: '#CCC',
     flex: 1,
   },
   groupName: {
+    flex: 5,
     fontSize: 20,
     fontWeight: 'bold',
   },
   removeGroup: {
-    fontSize: 14
+    flex: 1,
+    backgroundColor: '#8cb3d9',
+    height: 50,
+    width: 50,
+    color: '#005dff'
   },
   bottomStyle: {
     bottom: 0,
     position: 'absolute',
+
     width: '100%',
     alignItems: 'center',
   },
